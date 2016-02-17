244c2027d28861931b1fb9994640e9883035ea5d
huangjin <huangjin@RomeoSkydeMac-mini.local>
2016-02-17 17:41:44 +0800

    新建

---
+++ HJRESideMenu/HJRESideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJRESideMenu
+//
+//  Created by huangjin on 16/2/17.
+//  Copyright © 2016年 huangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJRESideMenu/HJRESideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJRESideMenu
+//
+//  Created by huangjin on 16/2/17.
+//  Copyright © 2016年 huangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
